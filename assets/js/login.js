2fd279aa3ff837b159e3db60f766760776fd2cb3
Murilo Carvalho <murilo.baleeiro@hotmail.com>
2022-03-19 14:38:13 -0300

    Correção do JavaScript que valida os campos de entrada da tela de Login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,48 +1,69 @@
-const btnShowPassword = document.querySelector("#lblPassword");
-const chkRemember = document.querySelector("#chkRememberMe");
-const btnLogin = document.querySelector("#btnLogin");
+const elements = {
+  user: document.querySelector("#txtUser"),
+  pass: document.querySelector("#txtPassword"),
+  btnShowPassword: document.querySelector("#lblPassword"),
+  chkRemember: document.querySelector("#chkRememberMe"),
+  btnAccess: document.querySelector("#btnLogin"),
+};
 
-const user = document.querySelector("#txtUser");
-const pass = document.querySelector("#txtPassword");
-
-btnShowPassword.addEventListener("click", () => {
-  if (pass.type === "password") {
-    pass.type = "text";
-    btnShowPassword.innerHTML = `<i id="hidden" class="fa-solid fa-eye-slash"></i>`;
+elements.btnShowPassword.addEventListener("click", () => {
+  if (elements.pass.type === "password") {
+    elements.pass.type = "text";
+    elements.btnShowPassword.innerHTML = `<i id="show" class="fa-solid fa-eye">`;
   } else {
-    pass.type = "password";
-    btnShowPassword.innerHTML = `<i id="show" class="fa-solid fa-eye">`;
+    elements.pass.type = "password";
+    elements.btnShowPassword.innerHTML = `<i id="hidden" class="fa-solid fa-eye-slash"></i>`;
   }
 });
 
-btnLogin.addEventListener("click", () => {
-  window.location.assign("./index.html");
-  if (user.value === "Murilo") {
-    return null;
-  } else {
-    user.classList.add("error");
-    setTimeout(() => {
-      user.classList.remove("error");
-    }, 2500);
-  }
-});
-
-chkRemember.addEventListener("click", () => {
+elements.chkRemember.addEventListener("click", () => {
   const tempUser = {
-    usuario: user.value,
-    senha: pass.value,
-    remember: chkRemember.value,
+    username: elements.user.value,
+    password: elements.pass.value,
+    remember: elements.chkRemember.value,
   };
 
-  chkRemember.checked
+  elements.chkRemember.checked
     ? localStorage.setItem("tempUser", JSON.stringify(tempUser))
     : localStorage.removeItem("tempUser", JSON.stringify(tempUser));
 });
 
 window.addEventListener("load", () => {
-  const tempUser = JSON.parse(localStorage.getItem("tempUser")) ?? null;
-  console.log(tempUser.usuario, tempUser.senha);
-  user.value = tempUser.usuario;
-  pass.value = tempUser.senha;
-  tempUser.remember == "on" ? (chkRemember.checked = true) : (chkRemember.checked = false);
+  const tempUser = JSON.parse(localStorage.getItem("tempUser"));
+  if (tempUser != null) {
+    elements.user.value = tempUser.username;
+    elements.pass.value = tempUser.password;
+    tempUser.remember == "on" ? (elements.chkRemember.checked = true) : (elements.chkRemember.checked = false);
+  }
+});
+
+function error(element) {
+  element.classList.add("error");
+  setTimeout(() => {
+    element.classList.remove("error");
+  }, 2000);
+}
+
+function loadHome() {
+  setTimeout(() => {
+    window.location.assign("/home.html");
+  }, 1500);
+}
+
+elements.btnAccess.addEventListener("click", () => {
+  const tempUser = {
+    username: elements.user.value,
+    password: elements.pass.value,
+  };
+
+  if ((tempUser.username === "user") & (tempUser.password === "1234")) {
+    console.warn("Acessou!");
+    loadHome();
+  } else {
+    if (tempUser.username !== "user") {
+      error(elements.user);
+    } else {
+      error(elements.pass);
+    }
+  }
 });
